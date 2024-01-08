87839e593bb9c38f3dc7868c045fefbf62ef7e37
jerusan <jerusan@hotmail.com>
2024-01-07 23:50:28 -0800

    allowed all origin

--- backend/__init__.py
+++ backend/__init__.py
@@ -5,7 +5,7 @@ import os
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)
+    CORS(app, origins='*')
 
     check_environment_variables(["PERPLEXITY_API_KEY", "OPENAI_API_KEY", "OPENAI_CRITIQ_ASSISTANT_ID"])
 
